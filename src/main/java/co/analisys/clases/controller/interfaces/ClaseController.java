364c196125cde479b58b18621db36f0cd7f1723c
juanex2001 <juanseg201@gmail.com>
2025-09-07 11:48:58 -0500

    feat: added swagger dependencies and operations in controller interface

--- src/main/java/co/analisys/clases/controller/interfaces/ClaseController.java
+++ src/main/java/co/analisys/clases/controller/interfaces/ClaseController.java
@@ -4,15 +4,30 @@ import co.analisys.clases.dto.ClaseOutDTO;
 import co.analisys.clases.model.Clase;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
 @RequestMapping("/clases")
+@Tag(name = "Clase", description = "Operaciones relacionadas con las clases")
 public interface ClaseController {
 
     @PostMapping()
+    @Operation(summary = "Crear una nueva clase")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Clase creada exitosamente"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Solicitud inválida")
+    })
     public Clase crearClase(@RequestBody Clase clase);
     
     @GetMapping()
+    @Operation(summary = "Listar todas las clases")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Clases listadas exitosamente"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "No se encontraron clases")
+    })
     public List<ClaseOutDTO> listarClases();
 
 }
